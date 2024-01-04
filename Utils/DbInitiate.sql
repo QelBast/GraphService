43c48691305f97ed99e0baf6abee874a6bef187a
SolW <kllykov@outlook.com>
2024-01-04 20:58:46 +0300

    Обновление таблицы Files в скрипте

--- Utils/DbInitiate.sql
+++ Utils/DbInitiate.sql
@@ -1,9 +1,10 @@
 CREATE TABLE "Files"
 (
-    "Id" BIGINT PRIMARY KEY,
+    "Id" guid PRIMARY KEY,
     "Edges" TEXT,
     "Text" TEXT,
     "EdgesColor" TEXT,
+    "NodesColor" TEXT,
     "CreationDateTime" timestamp with time zone,
     "ModifyDateTime" timestamp with time zone,
     "IsDeleted" BOOLEAN NOT NULL DEFAULT false
